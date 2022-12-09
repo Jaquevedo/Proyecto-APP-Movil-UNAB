7698a1fbe709f63114e44263e91ac75997013e04
jaquevedo <113221675+jaquevedoUSA@users.noreply.github.com>
2022-12-09 16:43:50 -0500

    Pantalla 1

---
+++ app/src/main/java/com/example/spr/password/CambioPassword.java
@@ -0,0 +1,16 @@
+package com.example.spr.password;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.spr.R;
+
+public class CambioPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambio_password);
+    }
+}
