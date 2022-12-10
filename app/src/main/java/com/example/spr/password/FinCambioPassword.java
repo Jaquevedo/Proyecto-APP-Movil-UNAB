ee6fcf08a54a5a114bbd3f310e3ca95a30a1d644
jaquevedo <113221675+jaquevedoUSA@users.noreply.github.com>
2022-12-10 13:05:48 -0500

    Pantallas completas

---
+++ app/src/main/java/com/example/spr/password/FinCambioPassword.java
@@ -0,0 +1,16 @@
+package com.example.spr.password;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spr.R;
+
+public class FinCambioPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fin_cambio_password);
+    }
+}
---
+++ app/src/main/java/com/example/spr/password/RestaurarPassword.java
@@ -0,0 +1,16 @@
+package com.example.spr.password;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spr.R;
+
+public class RestaurarPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurar_password);
+    }
+}
